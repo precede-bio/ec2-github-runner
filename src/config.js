e362c4f695d5dddc159a3156ba91b9384b78565b
Jonathan LEI <me@xjonathan.dev>
2023-12-15 20:37:33 +0000

    Add option for custom root volume storage size

--- src/config.js
+++ src/config.js
@@ -15,6 +15,7 @@ class Config {
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       preRunnerScript: core.getInput('pre-runner-script'),
+      storageSize: core.getInput('storage-size'),
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
